528e66eeddda378e58b8d9ff75950470209ce3f9
AxelMQ <axelmamaniquispia@gmail.com>
2026-06-11 06:56:21 -0400

    refactor[BACK-PEDIDO]: Configurar cors

--- backend/ms-pedidos/src/main.ts
+++ backend/ms-pedidos/src/main.ts
@@ -5,8 +5,15 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const port = process.env.PORT ?? 3001;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   logger.log(`MS2 Pedidos y Delivery running on port ${port}`);
 }
 bootstrap();
